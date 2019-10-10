22d57aade343ee721a1de743a4c9a84b63fde745
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-10-10 18:14:04 -0400

    Configuração de Layout e Frameworks

--- TDSTecnologia.FaceAlbum.Web/Startup.cs
+++ TDSTecnologia.FaceAlbum.Web/Startup.cs
@@ -16,6 +16,7 @@ namespace TDSTecnologia.FaceAlbum.Web
         {
             app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
         }
     }
 }
