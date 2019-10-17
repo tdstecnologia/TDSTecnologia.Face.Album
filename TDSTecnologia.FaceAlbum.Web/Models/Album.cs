d7a120cfb8ee28bd377b9aeea6f7c95c57a13473
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-10-17 12:15:27 -0400

    ADD Mapeamento de Colunas BD

--- TDSTecnologia.FaceAlbum.Web/Models/Album.cs
+++ TDSTecnologia.FaceAlbum.Web/Models/Album.cs
@@ -1,27 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TDSTecnologia.FaceAlbum.Web.Models
 {
+    [Table("tb01_album")]
     public class Album
     {
+        [Key]
+        [Column("id")]
         public int AlbumId { get; set; }
 
         [Required(ErrorMessage = "Campo obrigatório")]
         [StringLength(50, ErrorMessage = "Limite de 50 caracteres")]
+        [Column("destino")]
         public string Destino { get; set; }
 
+        [Column("fototopo")]
         public string FotoTopo { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         [Required(ErrorMessage = "Campo obrigatório")]
+        [Column("dt_inicio")]
         public DateTime Inicio { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         [Required(ErrorMessage = "Campo obrigatório")]
+        [Column("dt_fim")]
         public DateTime Fim { get; set; }
 
         //public ICollection<Imagem> Imagens { get; set; }
--- TDSTecnologia.FaceAlbum.Web/Models/Imagem.cs
+++ TDSTecnologia.FaceAlbum.Web/Models/Imagem.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace TDSTecnologia.FaceAlbum.Web.Models
 {
+    [Table("tb01_imagem")]
     public class Imagem
     {
+        [Key]
+        [Column("id")]
         public int ImagemId { get; set; }
 
-
+        [Column("link")]
         public string Link { get; set; }
 
+        [Column("album")]
         public int AlbumId { get; set; }
+
         public Album Album { get; set; }
     }
 }
